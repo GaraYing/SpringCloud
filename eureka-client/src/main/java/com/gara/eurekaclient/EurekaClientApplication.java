92ce64b758065800bdf0aa051e3b77d5d8058b45
GaraYing <984851486@qq.com>
2019-08-24 17:52:10 +0800

    -- customized config properties on spring cloud

--- eureka-client/src/main/java/com/gara/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/gara/eurekaclient/EurekaClientApplication.java
@@ -3,11 +3,21 @@ package com.gara.eurekaclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.bootstrap.config.PropertySourceLocator;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -25,5 +35,26 @@ public class EurekaClientApplication {
     @RequestMapping("/hi")
     public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
         return "hi " + name + " ,i am from port:" + port;
+    }
+
+    /**
+     * 实现自定义配置
+     * 1. 实现 PropertySourceLocator
+     * 2. spring bean 管理
+     * 3. 实现 propertySource
+     * 4. 定义并且配置META-INF/spring.properties
+     */
+    @Configuration
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public static class MyPropertySourceLocator implements PropertySourceLocator{
+
+        @Override
+        public PropertySource<?> locate(Environment environment) {
+            Map<String, Object> source = new HashMap<>();
+            source.put("server.port", "9090");
+
+            MapPropertySource propertySource = new MapPropertySource("my-property-source", source);
+            return propertySource;
+        }
     }
 }
--- note.md
+++ note.md
@@ -1,7 +1,16 @@
 ## spring 核心
 
-`ApplicationEvent`
+1. `ApplicationEvent`
 
-`ApplicationListener`
+2. `ApplicationListener`
 
-`ConfigFileApplicationListener`
+3. `ConfigFileApplicationListener`
+
+4. `BootstraptApplicationListener`  
+    1). 负责加载`bootstrapt.properties` and `application.yml`;     
+    2). 负责初始化Bootstrap ApplicationContext ID = 'bootstrap';   
+    3) `BootstraptApplicationListener` 优先级高于`ConfigFileApplicationListener` (by `DEFAULT_ORDER`)
+     
+5. `ConfigurableApplicationContext`
+
+5. `AbstractApplicationContext` onFresh()核心方法
