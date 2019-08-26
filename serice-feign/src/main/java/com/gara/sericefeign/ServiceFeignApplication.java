876b0638c7cdf0ca2f0cf2cce3c5d999ea4b9ca0
GaraYing <984851486@qq.com>
2019-08-26 15:45:24 +0800

    -- eureka instances displayed as IP:PORT

--- serice-feign/src/main/java/com/gara/sericefeign/ServiceFeignApplication.java
+++ serice-feign/src/main/java/com/gara/sericefeign/ServiceFeignApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SericeFeignApplication {
+public class ServiceFeignApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SericeFeignApplication.class, args);
+        SpringApplication.run(ServiceFeignApplication.class, args);
     }
 }
