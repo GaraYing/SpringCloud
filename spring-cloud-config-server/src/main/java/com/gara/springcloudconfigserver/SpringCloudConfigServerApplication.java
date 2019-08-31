687333893bef6c2f42a9c3b46d5dcba365313757
GaraYing <984851486@qq.com>
2019-08-31 18:33:33 +0800

    -- spring cloud config server and client

---
+++ spring-cloud-config-server/src/main/java/com/gara/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.gara.springcloudconfigserver;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+import javax.validation.Valid;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+    }
+}
