3d9bff3cce3f17b3949b09eaf8285281c590587f
GaraYing <984851486@qq.com>
2019-10-23 14:42:22 +0800

    -- polish code

--- eureka-consumer-ribbon/src/main/java/com/gara/eurekaconsumerribbon/EurekaConsumerRibbonApplication.java
+++ eureka-consumer-ribbon/src/main/java/com/gara/eurekaconsumerribbon/EurekaConsumerRibbonApplication.java
@@ -1,15 +1,13 @@
 package com.gara.eurekaconsumerribbon;
 
-import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableHystrix
-@Hystrix
+@EnableEurekaClient
 @SpringBootApplication
 public class EurekaConsumerRibbonApplication {
 
