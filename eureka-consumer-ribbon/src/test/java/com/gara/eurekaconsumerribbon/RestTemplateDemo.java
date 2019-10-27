3d69276880bac0668fd92b86a5cfe4642a79a640
GaraYing <984851486@qq.com>
2019-10-27 18:34:00 +0800

    -- learn RestTemplate

---
+++ eureka-consumer-ribbon/src/test/java/com/gara/eurekaconsumerribbon/RestTemplateDemo.java
@@ -0,0 +1,16 @@
+package com.gara.eurekaconsumerribbon;
+
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+public class RestTemplateDemo {
+
+    public static void main(String[] args) {
+        RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
+        Map<String, Object> data = restTemplate.getForObject("http://localhost:1001/actuator/env", Map.class);
+
+        System.out.println(data);
+    }
+}
